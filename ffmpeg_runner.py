643b9ac76c33ca2c5704f8d57c0d909c0b8028a7
Marty Ballard <marty331@gmail.com>
2021-08-26 16:48:34 -0500

    ready to deploy to test server

--- ffmpeg_runner.py
+++ ffmpeg_runner.py
@@ -24,6 +24,7 @@ class FfmpegRunner:
         self.aws_secret_access_key = config('AWS_SECRET_ACCESS_KEY')
         self.region_name = config('REGION_NAME')
         self.endpoint_url = config('ENDPOINT_URL')
+        self.current_video = None
 
     def aws_client(self):
         session = boto3.session.Session()
@@ -47,6 +48,7 @@ class FfmpegRunner:
     def retrieve_video_files(self, client):
         # print(f"location {self.location}")
         # video_files = [file for file in glob.glob(self.location + "*.mp4")]
+        current_directory = os.getcwd()
         video_files = self.aws_bucket(client=client)
         print(f"video files {video_files} num {len(video_files)}")
         while True:
@@ -55,15 +57,26 @@ class FfmpegRunner:
                 # get duration
                 # encode video
                 # wait until time expired and delete segments
-                movie_file = self.aws_download_file(client, movie)
-                self.run_ffprobe(self.get_video_duration(input_file=movie_file))
-                # seconds = self.run_time.decode("utf-8").replace('FORMAT', '').replace('[', '').replace(']', '').replace('/', '')
-                print(f"search run time {float(self.run_time)}")
-                # self.run_parse(self.parse_video_duration(output_file=self.run_time.decode("utf-8") ))
-                self.runFFmpeg(self.buildFFmpegCommand(input_file=movie_file))
+                self.aws_download_file(client, movie)
+                times_searched = 0
+                while times_searched < 3:
+                    file_list = os.listdir(current_directory)
+                    parent_dir = os.path.dirname(current_directory)
+                    if movie in file_list:
+                        self.current_video = movie
+                        self.run_ffprobe(self.get_video_duration(input_file=movie))
+                        # seconds = self.run_time.decode("utf-8").replace('FORMAT', '').replace('[', '').replace(']', '').replace('/', '')
+                        print(f"search run time {float(self.run_time)}")
+                        # self.run_parse(self.parse_video_duration(output_file=self.run_time.decode("utf-8") ))
+                        self.runFFmpeg(self.buildFFmpegCommand(input_file=movie))
+                        break
+                    else:
+                        times_searched += 1
+                        print('movie not found')
+                        sleep(2)
 
-    async def aws_download_file(self, client, movie):
-        movie_file = await client.download_file(
+    def aws_download_file(self, client, movie):
+        movie_file = client.download_file(
             'martystestspace',
             movie,
             f'/Users/marty331/PycharmProjects/ffmpeg_test/{movie}')
@@ -77,23 +90,13 @@ class FfmpegRunner:
         user_input_dict["audio_codec"] = "aac"
         user_input_dict["audio_bitrate"] = "196k"
         user_input_dict["sample_rate"] = "44100"
-        user_input_dict["encoding_speed"] = "fast"
+        user_input_dict["encoding_speed"] = "slow"
+        user_input_dict["speed"] = "21"
         user_input_dict["crf"] = "22"
         user_input_dict["frame_size"] = "1280x720"
-        user_input_dict["hls_time"] = "1"
-        user_input_dict["hls_list_size"] = "3"
+        user_input_dict["hls_time"] = "5"
+        user_input_dict["hls_list_size"] = "5"
         user_input_dict["start_number"] = "1"
-        # user_input_dict["output_file"] = filterInput("Output File (default = /Users/marty331/Movies/videos/second/index.m3u8): ", "/Users/marty331/Movies/videos/second/index.m3u8")
-        # user_input_dict["video_codec"] = filterInput("Video Codec (default = libx264): ", "libx264")
-        # user_input_dict["audio_codec"] = filterInput("Audio Codec (default = aac): ", "aac")
-        # user_input_dict["audio_bitrate"] = filterInput("Audio Bitrate (default = 196k): ", "196k")
-        # user_input_dict["sample_rate"] = filterInput("Sample Rate (default = 44100): ", "44100")
-        # user_input_dict["encoding_speed"] = filterInput("Encoding Speed: (default = fast): ", "fast")
-        # user_input_dict["crf"] = filterInput("Constant Rate Factor: (default = 22): ", "22")
-        # user_input_dict["frame_size"] = filterInput("Frame Size (default = 1280x720): ", "1280x720")
-        # user_input_dict["hls_time"] = filterInput("HLS Time (defualt = 10): ", "10")
-        # user_input_dict["hls_list_size"] = filterInput("HLS List Size = (default = 20): ", "20")
-        # user_input_dict["start_number"] = filterInput("Start Number = (defualt = 1): ", "1")
 
         self.user_input = user_input_dict
         return user_input_dict
@@ -138,6 +141,8 @@ class FfmpegRunner:
             final_user_input["video_codec"],
             "-preset",
             final_user_input["encoding_speed"],
+            "-speed",
+            final_user_input["speed"],
             "-crf",
             final_user_input["crf"],
             "-sc_threshold",
@@ -171,21 +176,45 @@ class FfmpegRunner:
 
         return commands_list
 
-    def remove_ts_files(self):
-        print(f"user output file {self.user_input}")
+    def get_file_path_of_assets(self):
         raw_file_path = self.user_input['output_file'].split('/')
         final_file_path = ''
         for p in raw_file_path[1:-1]:
             final_file_path = final_file_path + '/' + p
+        return final_file_path
+
+    def remove_ts_files(self):
+        print(f"user output file {self.user_input}")
+        final_file_path = self.get_file_path_of_assets()
         for filename in os.listdir(final_file_path):
             file_path = os.path.join(final_file_path, filename)
             file_type = file_path.split('.')[-1:]
             try:
-                if os.path.isfile(file_path) and file_type == '.ts':
+                print(f"file path {os.path.isfile(file_path)} type {file_type[0]} {file_type[0] == 'ts'}")
+                if os.path.isfile(file_path) and file_type[0] == 'ts':
+                    os.unlink(file_path)
+                    print(f"delete {file_path}")
+                if os.path.isfile(file_path) and file_type[0] == 'm3u8':
                     os.unlink(file_path)
                     print(f"delete {file_path}")
             except Exception as e:
                 return print(f'Failed to delete {file_path}. Reason: {e}')
+        return print("Files removed successfully.")
+
+    def remove_movie_file(self):
+        final_file_path = os.getcwd()
+        print(f"remove movie cwd {final_file_path}")
+        for filename in os.listdir(final_file_path):
+            file_path = os.path.join(final_file_path, filename)
+            file_type = file_path.split('.')[-1:]
+            try:
+                print(f"file path {os.path.isfile(file_path)} type {file_type[0]} {file_type[0] == 'mp4'}")
+                if os.path.isfile(file_path) and file_type[0] == 'mp4':
+                    os.unlink(file_path)
+                    print(f"delete {file_path}")
+            except Exception as e:
+                return print(f'Failed to delete {file_path}. Reason: {e}')
+
         return print("Files removed successfully.")
 
     def run_ffprobe(self, commands):
@@ -210,5 +239,13 @@ class FfmpegRunner:
             wait_time = float(self.run_time) - end
             sleep(wait_time)
             self.remove_ts_files()
+            self.remove_movie_file()
         else:
             print("There was an error running your FFmpeg script")
+
+if __name__ == "__main__":
+    from ffmpeg_runner import FfmpegRunner
+    ru = FfmpegRunner()
+    client = ru.aws_client()
+    video_files = ru.aws_bucket(client=client)
+    ru.retrieve_video_files(client)
